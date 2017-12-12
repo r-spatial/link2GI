97c0bedb5d0e07032032d460c53f0f6a1f445812
gisma <gisma@gis-ma.org>
2017-12-12 10:03:56 +0100

    fix pathstring

--- R/linkGRASS.R
+++ R/linkGRASS.R
@@ -111,8 +111,9 @@ linkGRASS7 <- function(x = NULL,
                       resolution=NULL) {
   # if no spatial object AND no extent AND no existing GRASS dbase is provided stop
   if (is.null(x) & is.null(spatial_params) & is.null(location)) {
-    stop("You MUST provide a raster* or sp* object, Did not found any of them so stopped.")
+    stop("You MUST provide a raster*, sp* object or manually the extent of the loccation...\n, Did not found any of them so stopped.")
   }
+  if ((class(x)=="character") & (substr(x,1,5)!= "+proj"))  x <- raster::raster(x)
   # search for GRASS on your system
   if (Sys.info()["sysname"] == "Windows") {
     home <- Sys.getenv("USERPROFILE")

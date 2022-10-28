215c718c2a84a4d5eb34331e090456d0945492c3
gisma <reudenbach@uni-marburg.de>
2022-10-28 14:17:49 +0200

    #57

--- R/parseOTB.R
+++ R/parseOTB.R
@@ -76,15 +76,17 @@ parseOTBFunction <- function(algo=NULL,gili=NULL) {
   if (algo != "" & otb$exist){
     system("rm otb_module_dump.txt",intern = FALSE,ignore.stderr = TRUE)
     if (!identical(grep(path_OTB,pattern = "OTB-8."), integer(0) )){
-      system(file.path(dirname(path_OTB),"otbenv.profile")) 
+      system(file.path(dirname(path_OTB[[1]]),"otbenv.profile")) 
       system(paste0("otbcli_",algo," -help >> " ,file.path(R.utils::getAbsolutePath(tempdir()),paste0("otb_module_dump.txt 2>&1")))) 
       } else {
 
     ifelse(Sys.info()["sysname"]=="Windows",
-           system(paste0(file.path(R.utils::getAbsolutePath(utils::shortPathName(path_OTB)),paste0("otbcli_",algo))," -help >> " ,file.path(R.utils::getAbsolutePath(tempdir()),paste0("otb_module_dump.txt 2>&1")))), 
-           system(paste0(file.path(R.utils::getAbsolutePath(path_OTB),paste0("otbcli_",algo))," -help >> " ,file.path(R.utils::getAbsolutePath(tempdir()),paste0("otb_module_dump.txt 2>&1"))))
+           system(paste0("env -i ", path_OTB, "otbcli ", algo, " -help >> ", file.path(R.utils::getAbsolutePath(tempdir()), paste0("otb_module_dump.txt 2>&1")))),
+           #system(paste0(file.path(R.utils::getAbsolutePath(utils::shortPathName(path_OTB)),paste0("otbcli_",algo))," -help >> " ,file.path(R.utils::getAbsolutePath(tempdir()),paste0("otb_module_dump.txt 2>&1")))), 
+           system(paste0("env -i ", path_OTB, "otbcli ", algo, " -help >> ", file.path(R.utils::getAbsolutePath(tempdir()), paste0("otb_module_dump.txt 2>&1"))))
+           #system(paste0(file.path(R.utils::getAbsolutePath(path_OTB),paste0("otbcli_",algo))," -help >> " ,file.path(R.utils::getAbsolutePath(tempdir()),paste0("otb_module_dump.txt 2>&1"))))
            )}
-    
+           system(paste0("env -i ", path_OTB, "otbcli ", algo, " -help >> ", file.path(R.utils::getAbsolutePath(tempdir()), paste0("otb_module_dump.txt 2>&1"))))
     txt <-readLines(file.path(tempdir(),"otb_module_dump.txt"))
     file.remove(file.path(tempdir(),"otb_module_dump.txt"))
     # Pull out the appropriate line
